4f6855848530c63372705f5d09c8a98092c6cbaa
navee-99 <naveenkamalesh0803@gmail.com>
2021-06-30 21:08:54 +0530

    add and remove data dynamiclly using scanner in linkedhas

--- 30.6.2021/src/assinment2/laibrary.java
+++ 30.6.2021/src/assinment2/laibrary.java
@@ -2,27 +2,33 @@ package assinment2;
 
 import java.util.Iterator;
 import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+
 
 public class laibrary {
 	public static void main (String a[]) {
 	
 		LinkedHashSet<String> link=new LinkedHashSet<String>();
-		link.add("java");
-		link.add("jsp");
-		link.add("jpa");
-		link.add("angular");
-		link.add("spring");
+		Scanner sc=new Scanner(System.in);
+		
+		link.add(sc.next());
+		link.add(sc.next());
+		link.add(sc.next());
+		
 		Iterator itr=link.iterator();
 		while(itr.hasNext()) {
           System.out.print(itr.next()+",");
 		}
           System.out.println();
 		
-		link.remove("jpa");
-		Iterator itr1=link.iterator();
-		while(itr1.hasNext()) {
-          System.out.print(itr1.next()+",");
-		}
+			
+	   link.remove(sc.next());
+	   Iterator itr1=link.iterator();
+			  while(itr1.hasNext()) {
+			 System.out.print(itr1.next()+","); 
+			 }
+			 
 		
 	}
 
